1282e3d7e5fedc73daabf7cccce904fcd2c31a47
zennythh <shr0udd.02@gmail.com>
2026-03-16 18:55:39 +0800

    ayokonapo

--- ClientManagementSubsystem/userControls/inboundUserControl.Designer.cs
+++ ClientManagementSubsystem/userControls/inboundUserControl.Designer.cs
@@ -28,20 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.inboundFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel.Controls.Add(this.inboundFlowLayoutPanel, 0, 0);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 1;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(1622, 943);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // inboundFlowLayoutPanel
+            // 
+            this.inboundFlowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inboundFlowLayoutPanel.Location = new System.Drawing.Point(3, 3);
+            this.inboundFlowLayoutPanel.Name = "inboundFlowLayoutPanel";
+            this.inboundFlowLayoutPanel.Size = new System.Drawing.Size(480, 937);
+            this.inboundFlowLayoutPanel.TabIndex = 0;
             // 
             // inboundUserControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(245)))));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Controls.Add(this.tableLayoutPanel);
             this.Name = "inboundUserControl";
-            this.Size = new System.Drawing.Size(2433, 1451);
+            this.Size = new System.Drawing.Size(1622, 943);
+            this.tableLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.FlowLayoutPanel inboundFlowLayoutPanel;
     }
 }
